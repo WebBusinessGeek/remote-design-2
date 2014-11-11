f4f89dff698ace3c3371848628c44956f1f13f5b
Kevin Washington <kevw12188@gmail.com>
2014-11-11 15:23:03 -0500

    added tests for objects and slots

---
+++ tests/ObjectTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 11/11/14
+ * Time: 2:30 PM
+ */
+
+namespace Tests\ObjectTest;
+
+
+class ObjectTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_object_can_add_stateable_object()
+    {
+        
+    }
+
+}
--- tests/SlotTest.php
+++ tests/SlotTest.php
@@ -8,8 +8,14 @@
 
 class SlotTest extends PHPUnit_Framework_TestCase {
 
-    public function test_nothing()
+    public function test_slot_instantiates_with_a_controllable_object()
     {
+        $light = new \App\MyStuff\Object('light', 'kitchen');
+
+        $slot = new \App\MyStuff\Slot($light);
+
+        $this->assertEquals('light', $slot->object->getType());
+        $this->assertEquals('kitchen', $slot->object->getLocation());
 
     }
 
